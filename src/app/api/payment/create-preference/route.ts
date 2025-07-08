fbe688b7fb37043c1da20ec748b7f69644451783
Nicolas <nicolas.ignacio.h.r@gmail.com>
2025-07-08 00:48:52 -0400

    3--

--- src/app/api/payment/create-preference/route.ts
+++ src/app/api/payment/create-preference/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server'
 import { MercadoPagoConfig, Preference } from 'mercadopago'
 import { getServerSession } from 'next-auth'
-import { authOptions } from '@/app/api/auth/[...nextauth]/route'
+import { authOptions } from '@/lib/auth'
 import { prisma } from '@/lib/prisma'
 
 // Configurar Mercado Pago
